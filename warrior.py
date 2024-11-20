332d5ed767e83c48864940b7d5d9cde7f899a7d0
mbaroud244 <mbaroud474@gmail.com>
2024-11-20 15:04:14 +0100

    gravity
    Integrated the concept of gravity

--- warrior.py
+++ warrior.py
@@ -26,6 +26,7 @@ def get_background(name):
 
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
+    GRAVITY = 1
 
     def __init__(self, x, y, width, height):
         super().__init__()
@@ -35,6 +36,7 @@ class Player(pygame.sprite.Sprite):
         self.mask = None
         self.direction = "left"
         self.animation_count = 0
+        self.fall_count = 0
 
     def move(self, dx, dy):
         self.rect.x += dx
@@ -53,7 +55,10 @@ class Player(pygame.sprite.Sprite):
             self.animation_count = 0
 
     def loop(self, fps):
+        self.y_vel += min(1, (self.fall_count / fps ) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
+
+        self.fall_count += 1
 
     def draw(self, win):
         pygame.draw.rect(win, self.COLOR, self.rect)
